726328b0a509f2f7f76e0fdaa51e3a97f3205206
SagaanTheEpic <37469599+SagaanTheEpic@users.noreply.github.com>
2018-06-23 20:54:59 -0700

    Update BirdsESP.cs

--- StrandardDeep Cheat/BirdsESP.cs
+++ StrandardDeep Cheat/BirdsESP.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
@@ -18,7 +18,7 @@ namespace StrandardDeep_Cheat
             yield return new WaitForSeconds(Time);
         }
 
-       
+        Player localplayer = FindObjectsOfType<Player>()[0];
         private void OnGUI()
         {
             if (Menu.EntityESP == true)
@@ -28,21 +28,22 @@ namespace StrandardDeep_Cheat
                     IEnumerable<Interactive_BIRD> playerProxies = FindObjectsOfType<Interactive_BIRD>();
                     foreach (Interactive_BIRD playerProxy in playerProxies)
                     {
-                        Player localplayer = FindObjectsOfType<Player>()[0];
-                        Vector3 pos = playerProxy.transform.position;
-                        float dist = Vector3.Distance(pos, localplayer.transform.position);
-                        if (dist < Menu.EntityDist)
-                        {
-                            //Extremely useful
-                            Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
 
-                            if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
+                        Vector3 pos = playerProxy.transform.position;
+                        Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
+
+                        if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
+                        {
+                            float dist = Vector3.Distance(pos, localplayer.transform.position);
+                            if (dist < Menu.EntityDist)
                             {
+                                //Extremely useful
+
                                 //Interactive_FISHES proxyName = playerProxy.GetComponent<Interactive_FISHES>();
 
-                                string playerName = "Fishes";
+
                                 posScreen.y = Screen.height - (posScreen.y + 1f);
-                                Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("   " + playerName + string.Format(" [{0:0}m]", (object)dist)), Color.green);
+                                Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("   Birds" + string.Format(" [{0:0}m]", (object)dist)), Color.green);
                             }
                         }
                         SleepFor(0.300f);
