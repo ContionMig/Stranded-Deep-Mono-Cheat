4b9c65fbfecfd77a4f8c18dc65f4ce388e9ae825
SagaanTheEpic <37469599+SagaanTheEpic@users.noreply.github.com>
2018-06-23 20:56:45 -0700

    Update MissionBoardsESP.cs

--- StrandardDeep Cheat/MissionBoardsESP.cs
+++ StrandardDeep Cheat/MissionBoardsESP.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
@@ -18,6 +18,7 @@ namespace StrandardDeep_Cheat
             yield return new WaitForSeconds(Time);
         }
 
+        Player localplayer = FindObjectsOfType<Player>()[0];
         private void OnGUI()
         {
 
@@ -26,21 +27,18 @@ namespace StrandardDeep_Cheat
                 IEnumerable<InteractiveMissionTrigger> playerProxies = FindObjectsOfType<InteractiveMissionTrigger>();
                 foreach (InteractiveMissionTrigger playerProxy in playerProxies)
                 {
-                    Player localplayer = FindObjectsOfType<Player>()[0];
+
                     Vector3 pos = playerProxy.transform.position;
-                    float dist = Vector3.Distance(pos, localplayer.transform.position);
-                    if (dist < Menu.LoopDist)
+                    //Extremely useful
+                    Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
+
+                    if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
                     {
-                        //Extremely useful
-                        Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
-
-                        if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
+                        float dist = Vector3.Distance(pos, localplayer.transform.position);
+                        if (dist < Menu.LoopDist)
                         {
-                            //Interactive_FISHES proxyName = playerProxy.GetComponent<Interactive_FISHES>();
-
-                            string playerName = "Mission Board";
                             posScreen.y = Screen.height - (posScreen.y + 1f);
-                            Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("   " + playerName + string.Format(" [{0:0}m]", (object)dist)), Color.green);
+                            Render.DrawString(new Vector2(posScreen.x, posScreen.y), (" Mission Board" + string.Format(" [{0:0}m]", (object)dist)), Color.green);
                         }
                     }
                     SleepFor(0.300f);
