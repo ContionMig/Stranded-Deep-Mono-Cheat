97833c1716be3bee4fe37e96edfbf10736f5c94c
SagaanTheEpic <37469599+SagaanTheEpic@users.noreply.github.com>
2018-06-23 20:58:53 -0700

    Add files via upload

---
+++ StrandardDeep Cheat/BoarESP.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using Beam;
+using Beam.AI;
+
+namespace StrandardDeep_Cheat
+{
+    public class BoarESP : MonoBehaviour
+    {
+        private void Start()
+        {
+
+        }
+        IEnumerator SleepFor(float Time)
+        {
+            yield return new WaitForSeconds(Time);
+        }
+
+        Player localplayer = FindObjectsOfType<Player>()[0];
+        private void OnGUI()
+        {
+            if (Menu.EntityESP == true)
+            {
+                if (Menu.BoarESP)
+                {
+                    IEnumerable<InteractiveObject_BOAR> playerProxies = FindObjectsOfType<InteractiveObject_BOAR>();
+                    foreach (InteractiveObject_BOAR playerProxy in playerProxies)
+                    {
+
+                        Vector3 pos = playerProxy.transform.position;
+                        Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
+
+                        if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
+                        {
+                            float dist = Vector3.Distance(pos, localplayer.transform.position);
+                            if (dist < Menu.EntityDist)
+                            {
+                                //Extremely useful
+
+                                posScreen.y = Screen.height - (posScreen.y + 1f);
+                                Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("   Boar" + string.Format(" [{0:0}m]", (object)dist)), Color.green);
+                            }
+                        }
+                        SleepFor(0.300f);
+                    }
+                    SleepFor(0.300f);
+                }
+                SleepFor(0.300f);
+
+            }
+        }
+
+
+        private void Update()
+        {
+
+        }
+    }
+}
