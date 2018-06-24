259eb870c4b53132725fce0e64cbf38bc1f425a1
SagaanTheEpic <37469599+SagaanTheEpic@users.noreply.github.com>
2018-06-23 20:55:45 -0700

    Update FoodESP.cs

--- StrandardDeep Cheat/FoodESP.cs
+++ StrandardDeep Cheat/FoodESP.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
@@ -18,30 +18,27 @@ namespace StrandardDeep_Cheat
             yield return new WaitForSeconds(Time);
         }
 
+        Player localplayer = FindObjectsOfType<Player>()[0];
         private void OnGUI()
         {
             if (Menu.LootESP == true)
             {
                 if (Menu.FoodESP)
                 {
-                    IEnumerable<InteractiveObject_FOOD> FishProxies = FindObjectsOfType<InteractiveObject_FOOD>();
-                    foreach (InteractiveObject_FOOD FishProxy in FishProxies)
+                    IEnumerable<InteractiveObject_FOOD> playerProxies = FindObjectsOfType<InteractiveObject_FOOD>();
+                    foreach (InteractiveObject_FOOD playerProxy in playerProxies)
                     {
-                        Player localplayer = FindObjectsOfType<Player>()[0];
-                        Vector3 pos = FishProxy.transform.position;
-                        float dist = Vector3.Distance(pos, localplayer.transform.position);
-                        if (dist < Menu.LoopDist)
+                       
+                        Vector3 pos = playerProxy.transform.position;
+                        Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
+
+                        if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
                         {
-                            //Extremely useful
-                            Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
-
-                            if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
+                            float dist = Vector3.Distance(pos, localplayer.transform.position);
+                            if (dist < Menu.LoopDist)
                             {
-                                //Interactive_FISHES proxyName = FishProxy.GetComponent<Interactive_FISHES>();
-
-                                string playerName = "Food";
                                 posScreen.y = Screen.height - (posScreen.y + 1f);
-                                Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("   " + playerName + string.Format(" [{0:0}m]", (object)dist)), Color.green);
+                                Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("   Food" + string.Format(" [{0:0}m]", (object)dist)), Color.green);
                             }
                         }
                         SleepFor(0.300f);
