cb4cc5eda794e9fdcb319a6117c6131a1a803fc6
SagaanTheEpic <37469599+SagaanTheEpic@users.noreply.github.com>
2018-06-23 20:57:55 -0700

    Update SharkESP.cs

--- StrandardDeep Cheat/SharkESP.cs
+++ StrandardDeep Cheat/SharkESP.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
@@ -18,6 +18,7 @@ namespace StrandardDeep_Cheat
             yield return new WaitForSeconds(Time);
         }
 
+        Player localplayer = FindObjectsOfType<Player>()[0];
         private void OnGUI()
         {
             if (Menu.EntityESP == true)
@@ -27,21 +28,17 @@ namespace StrandardDeep_Cheat
                     IEnumerable<InteractiveObject_PISCUS> playerProxies = FindObjectsOfType<InteractiveObject_PISCUS>();
                     foreach (InteractiveObject_PISCUS playerProxy in playerProxies)
                     {
-                        Player localplayer = FindObjectsOfType<Player>()[0];
+                        
                         Vector3 pos = playerProxy.transform.position;
-                        float dist = Vector3.Distance(pos, localplayer.transform.position);
-                        if (dist < Menu.EntityDist)
+                        Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
+
+                        if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
                         {
-                            //Extremely useful
-                            Vector3 posScreen = Camera.main.WorldToScreenPoint(pos);
-
-                            if (posScreen.z > 0 & posScreen.y < Screen.width - 2)
+                            float dist = Vector3.Distance(pos, localplayer.transform.position);
+                            if (dist < Menu.EntityDist)
                             {
-                                //Interactive_FISHES proxyName = playerProxy.GetComponent<Interactive_FISHES>();
-
-                                string playerName = "Shark";
                                 posScreen.y = Screen.height - (posScreen.y + 1f);
-                                Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("   " + playerName + string.Format(" [{0:0}m]", (object)dist)), Color.green);
+                                Render.DrawString(new Vector2(posScreen.x, posScreen.y), ("  Shark" + string.Format(" [{0:0}m]", (object)dist)), Color.green);
                             }
                         }
                         SleepFor(0.300f);
